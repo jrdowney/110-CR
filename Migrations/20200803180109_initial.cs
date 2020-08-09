e68f28e76d5024f4c6117fbcc4da2d4d50246da7
jrdowney <64438369+jrdowney@users.noreply.github.com>
2020-08-09 11:19:10 -0500

    Add files via upload

---
+++ Migrations/20200803180109_initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PropertyRental.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Properties",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<string>(nullable: true),
+                    rooms = table.Column<int>(nullable: false),
+                    bath = table.Column<decimal>(nullable: false),
+                    price = table.Column<decimal>(nullable: false),
+                    sqrFeet = table.Column<int>(nullable: false),
+                    imageUrl = table.Column<string>(nullable: true),
+                    description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Properties", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Properties");
+        }
+    }
+}
